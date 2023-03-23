53c2c5f18ab77db4fd6dcff7613ce551fb21c519
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-23 19:02:27 +0530

    create stream to find the number having occurence more than one

--- src/main/java/springBootDemo/demo/DemoApplication.java
+++ src/main/java/springBootDemo/demo/DemoApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -20,6 +22,12 @@ public class DemoApplication {
 		List<Integer> l1 = Arrays.asList(4,5,2,7,8,9,3);
 		List<Integer> l2 =l1.stream().sorted().collect(Collectors.toList());
 		System.out.println(l2);
+
+		// find the  no. repeated in the list
+
+		List<Integer> l3 = List.of(2,2,2,3,4,5,4,6,7,8,8,8,9,3,3);
+		 Set<Integer> l4 = l3.stream().filter(e-> Collections.frequency(l3,e)>1).collect(Collectors.toSet());
+		System.out.println(l4);
 	}
 
 }
