22a61069fe3a149f5bb52394bc0aa4bd5505d087
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-23 18:55:53 +0530

    create a list and sorted the the data

--- src/main/java/springBootDemo/demo/DemoApplication.java
+++ src/main/java/springBootDemo/demo/DemoApplication.java
@@ -3,6 +3,10 @@ package springBootDemo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class DemoApplication {
 
@@ -12,6 +16,10 @@ public class DemoApplication {
 		System.out.println("abhishek srivastava");
 		System.out.println("this is git demo ");
 		System.out.println("this is second method");
+    // create a list and sort the element of the list
+		List<Integer> l1 = Arrays.asList(4,5,2,7,8,9,3);
+		List<Integer> l2 =l1.stream().sorted().collect(Collectors.toList());
+		System.out.println(l2);
 	}
 
 }
