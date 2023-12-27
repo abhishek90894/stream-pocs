06d84138e9661093903bb126ed93f62702481fcc
abhishek <abhisheksrivastava90894@gmail.com>
2023-12-27 12:47:56 +0530

    find even number in a list

---
+++ src/main/java/StreamPractice.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+
+        List<Integer> l1 = Arrays.asList(4,2,5,6,8,7,9,3);
+
+      List<Integer> l2 = l1.stream().filter(e->e%2==0).toList();
+        System.out.println(l2);
+    }
+}
